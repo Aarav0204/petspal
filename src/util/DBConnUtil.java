6c2432f2a3e3baa87ff1eac68e84d9eaf1bd255d
Aarav Srivastava <aaravsrivastava004@gmail.com>
2024-10-05 14:56:18 +0530

    Initial commit

---
+++ src/util/DBConnUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnUtil {
+     static Connection getConnection(String url, String user, String password) {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null; // Return null if there's an error
+        }
+    }
+}
